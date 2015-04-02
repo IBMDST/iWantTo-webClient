c5f79be89563470e1b8094f6c79f7c69bd435f9b
ruqr <sweetwqr@qq.com>
2015-04-02 21:38:39 +0800

    interest module

--- modules/share/shareController.js
+++ modules/share/shareController.js
@@ -4,13 +4,41 @@
 app.controller('ShareController',['$scope','loginService','shareService','sessionService',function($scope,loginService,shareService,sessionService){
     $scope.txt = 'page home';
     $scope.speechesList = [];
+
     $scope.logout=function(){
         loginService.logout();
     };
 
-    shareService.speeches().success(function(response){
+
+    shareService.speeches().success(function (response) {
+        shareService.showInterest().success(function (interest) {
+            $.each(response, function (index, content) {
+                if(interest.length > 0)
+                {
+                    var flag = 0;
+                    $.each(interest, function (index, interestContent) {
+                        if(interestContent.speechID == content.id)
+                        {
+                            eval("$scope.interest" + content.id + "=" + 'true');
+                            flag = 1;
+                            return false;
+                        }
+                    });
+                    if(flag == 0)
+                    {
+                        eval("$scope.interest" + content.id + "=" + 'false');
+                    }
+                }
+                else
+                {
+                    eval("$scope.interest" + content.id + "=" + 'false');
+                }
+            });
+        });
         $scope.speechesList = response;
     });
+
+
 
     $scope.iWantToShare = function(data){
         var sendShareMessage =
@@ -35,23 +63,23 @@ app.controller('ShareController',['$scope','loginService','shareService','sessio
         shareService.submitComment(sendCommentMessage,$scope);
     };
 
+    $scope.swapInterest = function(speechId){
+        var interestID;
+        var cssId = $('#'+speechId);
+        if(cssId.hasClass("glyphicon glyphicon-heart")){
+            shareService.interestId(speechId).success(function(response){
+                if(response.length>0)
+                {
+                    interestID = response[0].id;
+                }
+                shareService.deleteInterest(interestID,speechId, $scope);
 
-    $scope.speechInterest = function(speechId){
-        var sendInterestMessage =
-        {
-            'userID' : sessionService.get('uid'),
-            'speechID' : speechId,
-            'createdOn' : new Date().valueOf()
-        };
-        if(sessionService.get(speechId+'Interest') == speechId)
-        {
-            console.log('exits');
+            });
         }
-        else
+        if(cssId.hasClass("glyphicon glyphicon-heart-empty"))
         {
-            shareService.addInterest(sendInterestMessage,$scope);
-            var element = '#'+speechId;
-            $(element).addClass("swap");
+            shareService.addInterest(speechId, $scope);
+
         }
     };
 }]);
--- modules/share/shareService.js
+++ modules/share/shareService.js
@@ -19,7 +19,7 @@ app.factory('shareService',function($http,$location,sessionService){
 
         submitComment : function(data,scope) {
             var $promise = $http.post(rootUrl + '/speech-comments', data);
-            $promise.then(function (success) {
+            $promise.then(function () {
                 $http.get(rootUrl + '/speeches').success(function(response){
                     scope.speechesList = response;
                 });
@@ -30,17 +30,50 @@ app.factory('shareService',function($http,$location,sessionService){
             })
         },
 
-        addInterest : function(data,scope) {
-            var $promise = $http.post(rootUrl + '/speech-interested', data);
-            $promise.then(function (success) {
-                var speechID = success.data.speechID;
-                var sessionName = speechID + "Interest";
-                sessionService.set(sessionName,speechID);
+
+        showInterest : function(){
+           return $http({
+                method : 'GET',
+                url : rootUrl + '/speech-interested',
+                params : {'userID' : sessionService.get('uid')}
+            });
+        },
+
+        interestId : function(speechID){
+            return $http({
+                method : 'GET',
+                url : rootUrl + '/speech-interested',
+                params : {'userID' : sessionService.get('uid') , 'speechID' : speechID}
+            });
+        },
+
+        deleteInterest : function(ID,speechId, scope){
+            $http({
+                method : 'DELETE',
+                url : rootUrl + '/speech-interested/' + ID
+            }).success(function(){
+                $http.get(rootUrl + '/speeches').success(function(response){
+                    scope.speechesList = response;
+                    eval("scope.interest" + speechId + "=" + 'false');
+                });
                 $location.path('/share');
-            }, function (error) {
-                scope.commitInterestFailtext = error.errors;
-                $location.path('/share');
-            })
+            });
+        },
+
+        addInterest : function(speechID,scope){
+            $http({
+                method : 'POST',
+                url : rootUrl + '/speech-interested',
+                data : {'userID' : sessionService.get('uid') , 'speechID' : speechID, 'createdOn' : new Date().valueOf()}
+            }).success(
+                function(){
+                    $http.get(rootUrl + '/speeches').success(function(response){
+                        scope.speechesList = response;
+                        eval("scope.interest" + speechID + "=" + 'true');
+                    });
+                    $location.path('/share');
+                }
+            );
         }
     }
 });
