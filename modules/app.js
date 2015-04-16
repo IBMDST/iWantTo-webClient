55462452fe45cb4f2534d830a79808934590199b
ruqr <sweetwqr@qq.com>
2015-04-16 17:01:00 +0800

    change root url

--- modules/app.js
+++ modules/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('shareApp',['ngRoute']);
-//var rootUrl = 'http://share.in-sync.co';
-var rootUrl = 'http://share.in-sync.co:2403';
+var rootUrl = 'http://share.in-sync.co';
+//var rootUrl = 'http://share.in-sync.co:2403';
 app.config(['$routeProvider','$locationProvider','$httpProvider',function($routeProvider,$locationProvider,$httpProvider){
 
     $locationProvider.html5Mode(false);
